5c8a4c1328f5eddcd7f8c8b200695e026136de23
hjhj111 <2268716068@qq.com>
2022-08-30 22:35:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
+<!--
+ * @Author: hjhj111 2268716068@qq.com
+ * @Date: 2022-08-30 12:15:37
+ * @LastEditors: hjhj111 2268716068@qq.com
+ * @LastEditTime: 2022-08-30 17:11:38
+ * @FilePath: \note_book\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/main.jpg">
+  <NoteBook address="http://192.168.56.101:5000/notes"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NoteBook from './components/NoteBook.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NoteBook
   }
 }
 </script>
---
+++ src/components/NoteBook.vue
@@ -0,0 +1,100 @@
+<!--
+ * @Author: hjhj111 2268716068@qq.com
+ * @Date: 2022-08-30 12:15:37
+ * @LastEditors: hjhj111 2268716068@qq.com
+ * @LastEditTime: 2022-08-30 22:32:02
+ * @FilePath: \note_book\src\components\HelloWorld.vue
+ * @Description: 这是一个简单基于vue的在线备忘录,结合c++网络库Rester，内存数据库redis使用
+-->
+<template>
+  <div>
+    <h1>数据服务器地址: {{ address }}</h1>
+    <ol style="list-style-type:none">
+      <li v-for="note in notes" :key="note.text">
+        <h2>{{note.text}}</h2>
+        <button>完成待办</button>
+      </li>
+    </ol>
+    <!-- <form action={{this.adress}} method="post"> -->
+      <input type="text"  id="form" placeholder="添加待办……">
+      <input type="submit" value="确认添加" @click="addNote">
+    <!-- </form> -->
+  </div>  
+</template>
+
+<script>
+//import { setgroups } from 'process';
+import axios from 'axios'
+
+export default {
+  name: 'NoteBook',
+
+  data(){
+    return{
+      notes:[
+        {text:"hhhh"},
+        {text:"jjjjj"}
+      ],
+      Address: this.address
+    }    
+  },
+
+  props: {
+    address:{default: "no url for data"},    
+  },
+
+  created(){
+    this.getNotes()
+    console.log(this.notes)  
+  },
+  
+  methods:{   
+
+    getNotes: function(){       
+    console.log(this.address)        
+    var self=this//修改父对象，函数也是对象
+    axios.get(this.address).then(function(res){    
+    console.log(res.data)
+    self.notes=res.data
+    console.log(self.notes)  
+    }    
+    ).catch((err)=>{
+      console.log(err)      
+    })    
+    },
+
+    addNote: function(){
+      var self=this
+      console.log(document.getElementById("form").value)
+      axios({
+        method:"Post",
+        url:self.address,
+        headers:{
+          'Content-Type': 'text/plain'//application/json会乱码
+        },
+        data:{text:document.getElementById("form").value}
+        //data:"{\"text\":\"更加发奋建安费\"}"
+      }).then(function(res){
+        if(res.status==201){
+          //update
+          self.getNotes();
+          alert("添加完成");
+        }
+        else{
+          alert("添加失败");
+        }
+      })
+      
+    }
+    
+  }
+}
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
